ec7a71287aa06fee10b855271e7cd2b50d657f1e
coder1408 <ishanyateenjoshi@gmail.com>
2024-01-31 13:35:15 +0530

    Add GPU support and load pre-trained model

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# This is a sample Python script.
+import torch
+from braintumorref import CNN
+model = CNN()
+model.load_state_dict(torch.load('weights.pt'))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+model = model.to(device) # Set model to gpu
+model.eval();
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
